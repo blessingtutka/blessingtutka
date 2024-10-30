7311f4fde7554f6f11728a33b02ef2e915cb5a0a
blessingtutka <blessingtutka298@gmail.com>
2024-10-30 17:57:17 +0100

    refactor(hero): update hero section

--- src/components/layout/hero/hero-content.tsx
+++ src/components/layout/hero/hero-content.tsx
@@ -1,13 +1,17 @@
 import HeroImage from './hero-image';
 import { ArrowsAnimation } from '@/assets/icons';
+// import { Link } from 'react-router-dom';
 
 function HeroContent() {
     return (
-        <div className='flex h-[90%] pt-20 px-2 flex-col items-center'>
+        <div className='flex absolute bottom-3 width w-full h-[90%] pt-20 px-2 flex-col items-center'>
             <div>
                 <HeroImage />
                 <p className='text-white text-center pt-5 text-2xl'>
                     Hey, I'm <b> Benedicte Tuteka (Blessing Tutka)</b>
+                </p>
+                <p className='fancy text-white text-center pt-5 text-2xl'>
+                    <b>Sofware Engineer</b>
                 </p>
             </div>
             <ArrowsAnimation />
--- src/components/layout/hero/hero-image.tsx
+++ src/components/layout/hero/hero-image.tsx
@@ -2,7 +2,7 @@ import '../../../style/hero-moon.scss';
 
 function HeroImage() {
     return (
-        <div className='moon hero-image w-full lg:w-5/12 relative z-10'>
+        <div className='moon hero-image relative z-10'>
             <img src='/images/hero.png' className='hero-image-wrap  img-fluid' alt='' />
             <div className='star-1'></div>
             <div className='star-2'></div>
