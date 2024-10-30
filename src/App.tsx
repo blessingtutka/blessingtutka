6f10f3554e4992951ac306d7846dbaf709558562
blessingtutka <blessingtutka298@gmail.com>
2024-10-30 18:08:33 +0100

    refactor(app): add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Layout from './components/layout/layout';
-function App() {
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import './style/special.scss';
+
+const App = () => {
     return (
-        <>
+        <Router>
             <Layout />
-        </>
+        </Router>
     );
-}
+};
 
 export default App;
