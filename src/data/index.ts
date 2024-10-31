789b6f72aeed9e2615c6c76c550ff49f66e475d6
blessingtutka <blessingtutka298@gmail.com>
2024-10-31 23:28:45 +0100

    feat(data): add navlink data

---
+++ src/data/index.ts
@@ -0,0 +1,3 @@
+import navLinks from './nav-links';
+
+export { navLinks };
---
+++ src/data/nav-links.ts
@@ -0,0 +1,28 @@
+const navLinks: InavLink[] = [
+    {
+        label: 'Home',
+        link: '#home',
+    },
+    {
+        label: 'About',
+        link: '#about',
+    },
+    {
+        label: 'Services',
+        link: '#services',
+    },
+    {
+        label: 'Projects',
+        link: '#projects',
+    },
+    {
+        label: 'Skills',
+        link: '#skill',
+    },
+    {
+        label: 'Contact',
+        link: '#contact',
+    },
+];
+
+export default navLinks;
