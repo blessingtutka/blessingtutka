6f60efc3b80d8d4fb94f750b745b33624a258a77
blessingtutka <blessingtutka298@gmail.com>
2024-11-01 23:25:15 +0100

    feat(header): add nav brand

---
+++ src/components/layout/header/nav-brand.tsx
@@ -0,0 +1,9 @@
+function NavBrand() {
+    return (
+        <a href='index.html' className='nav-brand'>
+            <img src='./images/logo.png' alt='logo image' />
+        </a>
+    );
+}
+
+export default NavBrand;
