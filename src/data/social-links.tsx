4c8c5f70f972865976fe32351fece69df6f48a06
blessingtutka <blessingtutka298@gmail.com>
2024-11-12 23:38:59 +0100

    feat(data): add social links data

---
+++ src/data/social-links.tsx
@@ -0,0 +1,9 @@
+import { Github, Linkedin, Facebook, Mail, Twitter } from 'lucide-react';
+
+export const socialLinks = [
+    { label: 'GitHub', icon: <Github />, url: 'https://github.com/blessingtutka' },
+    { label: 'LinkedIn', icon: <Linkedin />, url: 'https://www.linkedin.com/in/b%C3%A9n%C3%A9dicte-tuteka-mukuta-5184802b2/' },
+    { label: 'Facebook', icon: <Facebook />, url: 'https://www.facebook.com/blessing.tutka' },
+    { label: 'Mail', icon: <Mail />, url: 'mailto:blessingtutka298@gmail.com' },
+    { label: 'Twitter', icon: <Twitter />, url: 'https://x.com/Blessingtutka' },
+];
