51d95ea5bca24ff8d97f76ccc15f942fd1076a7b
blessingtutka <blessingtutka298@gmail.com>
2024-11-20 23:48:35 +0100

    refactor(about): update avatar image

--- src/components/layout/about/about-avatar.tsx
+++ src/components/layout/about/about-avatar.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 function AboutAvatar({ avatar }: { avatar: string }) {
     return (
         <Avatar className='about-avatar w-12 h-12 border border-gray-300 rounded-full'>
-            <AvatarImage src={avatar} />
+            <AvatarImage src={avatar} className='object-cover' />
             <AvatarFallback className='text-blue-500'>BLES</AvatarFallback>
         </Avatar>
     );
