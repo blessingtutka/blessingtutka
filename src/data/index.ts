8633ec957e6119bcad15e57095ba11aef12a72b7
blessingtutka <blessingtutka298@gmail.com>
2024-11-21 23:47:17 +0100

    feat(data): add skill data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 import navLinks from './nav-links';
 import { socialLinks } from './social-links';
 import { about } from './about';
+import { skills } from './skill';
 
-export { navLinks, socialLinks, about };
+export { navLinks, socialLinks, about, skills };
---
+++ src/data/skill.tsx
@@ -0,0 +1,45 @@
+import { Monitor, Layout, Server, Network, FileText } from 'lucide-react';
+
+export const skills = [
+    {
+        title: 'Software Development',
+        subtitle: 'Experienced in both functional and OOP',
+        tools: ['python', 'js', 'c++'],
+        contentType: 'html',
+        icon: <Monitor className='icon' />,
+        gradient: 'magenta',
+    },
+    {
+        title: 'Frontend Development',
+        subtitle: 'Over 2 years of development experience in:',
+        tools: ['html', 'css', 'react'],
+        contentType: 'react',
+        icon: <Layout className='icon' />,
+        gradient: 'blue',
+    },
+    {
+        title: 'Backend Development',
+        subtitle: 'Over 2 years of development experience in:',
+        tools: ['python', 'django'],
+        contentType: 'python',
+        icon: <Server className='icon' />,
+        gradient: 'green',
+    },
+    {
+        title: 'API Development',
+        subtitle: 'Over 2 years of API development experience using:',
+        tools: ['django-rest', 'axios'],
+        contentType: 'api',
+        icon: <Network className='icon' />,
+        gradient: 'yellow',
+    },
+    {
+        title: 'WordPress Application',
+        subtitle: `Over 1 years of development experience in wordpress with the knowledge of its best theme, 
+        editor and plugin.Hence:`,
+        tools: ['elementor', 'gutenberg'],
+        contentType: 'wordpress',
+        icon: <FileText className='icon' />,
+        gradient: 'blue',
+    },
+];
