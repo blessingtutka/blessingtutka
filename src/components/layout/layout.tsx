50887f0773e6fa72e316eaaa59c863142fdeaf3f
blessingtutka <blessingtutka298@gmail.com>
2024-11-22 22:45:20 +0100

    feat(layout): add skill

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,7 @@
 import Header from './header';
 import Hero from './hero';
 import About from './about';
+import Skill from './skill';
 
 import '../../style/layout.scss';
 
@@ -10,6 +11,7 @@ function Layout() {
             <Header />
             <Hero />
             <About />
+            <Skill />
         </div>
     );
 }
