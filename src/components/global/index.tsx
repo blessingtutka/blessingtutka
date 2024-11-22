f0c621950d03c3ea7bfc5dfe4d44488991140696
blessingtutka <blessingtutka298@gmail.com>
2024-11-22 22:40:34 +0100

    feat(global): add section header

--- src/components/global/index.tsx
+++ src/components/global/index.tsx
@@ -1,3 +1,4 @@
 import Text from './text';
+import SectionHeader from './section-header';
 
-export { Text };
+export { Text, SectionHeader };
---
+++ src/components/global/section-header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+type SectionHeaderProps = {
+    image: string;
+    title: string;
+};
+
+const SectionHeader: React.FC<SectionHeaderProps> = ({ image, title }) => {
+    return (
+        <div className='flex m-2 justify-start'>
+            <div className='w-full md:w-4/5 mlg:w-1/2'>
+                <div className='section-header flex justify-center items-center mb-4'>
+                    <div className='section-header-img'>
+                        <div className='diamond'>
+                            <img src={image} alt={title} className='w-full h-full object-cover' />
+                        </div>
+                    </div>
+                    <h2 className='text-[#5F91FF] text-4xl font-semibold ml-8'>{title}</h2>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SectionHeader;
