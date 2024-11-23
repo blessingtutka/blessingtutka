2feff92dfd3ae97ca2ab7abc99aecab0de8b3cfb
blessingtutka <blessingtutka298@gmail.com>
2024-11-23 23:49:32 +0100

    refactor(type): update types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,13 @@
 interface InavLink {
     label: string;
+    name: string;
     link: string;
 }
+
+type ISkill = {
+    title: string;
+    subtitle: string;
+    tools: string[];
+    icon: string;
+    gradient: 'magenta' | 'blue' | 'green' | 'yellow';
+};
