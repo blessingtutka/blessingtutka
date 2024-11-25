9f0da4510b0ce1fc8e1ad1193e7baa6e278a221a
blessingtutka <blessingtutka298@gmail.com>
2024-11-25 23:24:55 +0100

    feat(data): add projects data

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,5 +2,6 @@ import navLinks from './nav-links';
 import { socialLinks } from './social-links';
 import { about } from './about';
 import { skills } from './skill';
+import { projects } from './project';
 
-export { navLinks, socialLinks, about, skills };
+export { navLinks, socialLinks, about, skills, projects };
---
+++ src/data/project.ts
@@ -0,0 +1,32 @@
+export const projects = [
+    {
+        title: 'ASHER',
+        tool: 'React/Express',
+        image: './images/projects/asher.png',
+        link: 'https://asher-ywpi.onrender.com',
+    },
+    {
+        title: 'MIC-DRC',
+        tool: 'html/css/js',
+        image: './images/projects/mic.png',
+        link: 'https://mic-0pkr.onrender.com/',
+    },
+    {
+        title: 'Gateway Export',
+        tool: 'Wordpress',
+        image: './images/projects/gateway.png',
+        link: 'https://dev-gatewayexportdrc.pantheonsite.io/',
+    },
+    {
+        title: 'CPRJ',
+        tool: 'html/css/js',
+        image: './images/projects/cprj.png',
+        link: 'https://blessingtutka.github.io/cprj/',
+    },
+    {
+        title: 'TWITTER CLONE',
+        tool: 'React/Express',
+        image: './images/projects/twitter-clone.png',
+        link: 'https://twitter-clone-nu-six.vercel.app/',
+    },
+];
