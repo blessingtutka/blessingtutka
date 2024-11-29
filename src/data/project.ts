83bf848bcd625af1bfec5eb839d4e3473b722bcf
blessingtutka <blessingtutka298@gmail.com>
2024-11-29 23:28:23 +0100

    refactor(data): update project data

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,37 +1,37 @@
 export const projects = [
     {
         title: 'ASHER',
-        tool: 'React/Express',
+        tags: ['React', 'Express'],
         image: './images/projects/asher.png',
         link: 'https://asher-ywpi.onrender.com',
     },
     {
         title: 'MIC-DRC',
-        tool: 'html/css/js',
+        tags: ['html', 'css', 'js'],
         image: './images/projects/mic.png',
         link: 'https://mic-0pkr.onrender.com/',
     },
     {
         title: 'Gtw Export',
-        tool: 'Wordpress',
+        tags: ['Wordpress'],
         image: './images/projects/gateway.png',
         link: 'https://dev-gatewayexportdrc.pantheonsite.io/',
     },
     {
         title: 'CPRJ',
-        tool: 'html/css/js',
+        tags: ['html', 'css', 'js'],
         image: './images/projects/cprj.png',
         link: 'https://blessingtutka.github.io/cprj/',
     },
     {
         title: 'Twitter Clone',
-        tool: 'React/Express',
+        tags: ['React', 'Express'],
         image: './images/projects/twitter-clone.png',
         link: 'https://twitter-clone-nu-six.vercel.app/',
     },
     {
         title: 'My Portfolio',
-        tool: 'React',
+        tags: ['React'],
         image: './images/projects/blessing-portfolio.png',
         link: 'https://blessingtutka-portfolio.vercel.app/',
     },
