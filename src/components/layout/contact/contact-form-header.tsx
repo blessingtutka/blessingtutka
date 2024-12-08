4a8d7db9faa5e4a101fb10a36094e8bc47d2f388
blessingtutka <blessingtutka298@gmail.com>
2024-12-08 23:18:20 +0100

    feat(contact): add contact form header

---
+++ src/components/layout/contact/contact-form-header.tsx
@@ -0,0 +1,13 @@
+function ContactFormHeader() {
+    return (
+        <div className='contact-form-heading'>
+            <h2 id='heading'>Get in touch</h2>
+            <p>
+                If you wanna get in touch, talk to me about a project collaboration or just say hi, fill up the awesome form below or send an email to
+                <b> blessingtutka298@gmail.com</b> and ~let's talk.
+            </p>
+        </div>
+    );
+}
+
+export default ContactFormHeader;
