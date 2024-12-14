f0b6a9b2637756844e02e48d0abf476ee68c2d38
blessingtutka <blessingtutka298@gmail.com>
2024-12-14 23:22:06 +0100

    chore(vite): set up env config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,13 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+    readonly VITE_APP_NAME: string;
+    readonly VITE_EMAIL_SUPPORT: string;
+    // Add more environment variables here
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
