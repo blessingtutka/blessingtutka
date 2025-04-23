9f5b76fa14b22793ac071ba49d634ccc22c6664a
blessingtutka <blessingtutka298@gmail.com>
2025-04-23 18:06:55 +0200

    refactor(project): update about project

--- src/components/layout/project/projects.tsx
+++ src/components/layout/project/projects.tsx
@@ -3,7 +3,7 @@ import ProjectCard from './project-card';
 const Projects: React.FC<{ projectsPaginated: IProject[] }> = ({ projectsPaginated }) => {
     return (
         <div className='flex flex-col items-center'>
-            <div className='flex flex-wrap gap-4 justify-center'>
+            <div className='projects flex flex-wrap gap-4 justify-center'>
                 {projectsPaginated.map((project, index) => (
                     <ProjectCard key={index} project={project} />
                 ))}
