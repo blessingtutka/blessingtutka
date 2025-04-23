7bb24aca44bf2f12030b1c2ee7541a34f78a97ff
blessingtutka <blessingtutka298@gmail.com>
2025-04-23 23:42:29 +0200

    feat(service): add email service

--- src/service/send-mail.ts
+++ src/service/send-mail.ts
@@ -1,1 +1,31 @@
-// API TO SEND MESSAGE HERE
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_ENDPOINT_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export interface ClientMailPayload {
+    email: string;
+    name: string;
+    message: string;
+    services: string[];
+}
+
+export const sendMail = async (payload: ClientMailPayload): Promise<void> => {
+    try {
+        await api.post('mail/send/email', payload);
+    } catch (error: any) {
+        const errorMsg = error.response?.data?.message || 'Failed to send email';
+        throw new Error(errorMsg);
+    }
+};
+
+const mailService = {
+    sendMail,
+};
+
+export default mailService;
