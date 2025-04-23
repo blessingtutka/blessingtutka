0d7a03560c090182f1bbe83ef564f47009eb1b80
blessingtutka <blessingtutka298@gmail.com>
2025-04-23 17:59:04 +0200

    perf(hero): update hero

--- src/components/layout/hero/hero-content.tsx
+++ src/components/layout/hero/hero-content.tsx
@@ -1,9 +1,10 @@
 import HeroImage from './hero-image';
 import { ArrowsAnimation } from '@/assets/icons';
 import HeroSocial from './hero-social';
-// import { Link } from 'react-router-dom';
+import { FileUser } from 'lucide-react';
 
 function HeroContent() {
+    const wavy = 'Software Engineer'.split('').map((char) => (char === ' ' ? '\u00A0' : char));
     return (
         <div className='flex absolute bottom-3 width w-full h-[90%] pt-20 px-2 flex-col items-center'>
             <div>
@@ -11,11 +12,29 @@ function HeroContent() {
                 <p className='text-white text-center pt-5 text-2xl'>
                     Hey, I'm <b> Benedicte Tuteka (Blessing Tutka)</b>
                 </p>
-                <p className='fancy text-white text-center pt-5 text-2xl'>
-                    <b>Sofware Engineer</b>
+                <p className='wave-text text-white text-center pt-5 text-2xl'>
+                    <b>
+                        {wavy.map((word) => (
+                            <span className='fancy'>{word}</span>
+                        ))}
+                    </b>
                 </p>
             </div>
             <HeroSocial />
+            <div className='sm:absolute top-1/2 left-4 sm:transform sm:-translate-y-1/2 mt-4 sm:mt-0'>
+                <a
+                    href='https://www.canva.com/design/DAGiqx7WR6Y/0yZwDYoARWoN-I_b__X7eQ/edit?ui=eyJEIjp7IlQiOnsiQSI6IlBCU2Z3ajN6OFJ6TjVNR3EifX19'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label={'my resume'}
+                    className='text-gray-600 hover:text-white transition-colors'
+                >
+                    <FileUser />
+                    <small>
+                        <b>CV</b>
+                    </small>
+                </a>
+            </div>
             <ArrowsAnimation />
         </div>
     );
