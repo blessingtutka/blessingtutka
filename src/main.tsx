3e2161b056c8ec22d6c76b640588773db8810f15
blessingtutka <blessingtutka298@gmail.com>
2025-04-23 17:56:50 +0200

    refactor(main): add animate css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import './style/global.scss';
+import 'animate.css';
+
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
