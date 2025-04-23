7c7f88c10f37cc183380e1f2a6e22037dc959062
blessingtutka <blessingtutka298@gmail.com>
2025-04-23 17:58:16 +0200

    refactor(global): update section header

--- src/components/global/section-header.tsx
+++ src/components/global/section-header.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 type SectionHeaderProps = {
     image: string;
     title: string;
+    className?: string;
 };
 
-const SectionHeader: React.FC<SectionHeaderProps> = ({ image, title }) => {
+const SectionHeader: React.FC<SectionHeaderProps> = ({ image, title, className }) => {
     return (
-        <div className='flex m-2 justify-start w-full'>
+        <div className={'flex m-2 my-10 justify-start w-full ' + className}>
             <div className='w-full md:w-4/5 mlg:w-1/2'>
                 <div className='section-header flex justify-center items-center mb-4'>
                     <div className='section-header-img'>
