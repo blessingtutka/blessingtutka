8a7ee8be6ed1f9bae8f45dc79367970fd7861cd5
blessingtutka <blessingtutka298@gmail.com>
2025-04-24 00:01:11 +0200

    refactor(contact): update title

--- src/components/layout/contact/index.tsx
+++ src/components/layout/contact/index.tsx
@@ -5,7 +5,7 @@ function Contact() {
     return (
         <div className='contact pb-4' id='contact'>
             <div className='container mx-auto px-4'>
-                <SectionHeader title='Say Hi' image='./images/hi.jpg' />
+                <SectionHeader title='Say Hi' image='./images/hi.jpg' className=' !justify-end' />
                 <div className='flex flex-col mlg:flex-row gap-3'>
                     <ContactInfo />
                     <ContactForm />
